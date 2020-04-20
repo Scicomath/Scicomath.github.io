6dc41c161ab7f95127935abbb83e7ff7104fadac
AI Xin <xin.ai@hairoutech.com>
2020-04-20 10:33:23 +0800

    remove post02

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,4 +1,3 @@
 var posts_array = [
-	["14/05/2019", "post 2", "@pathto(blog/post02)", "A project I recently finished"],
-	["03/02/2019", "post 1", "@pathto(blog/post01)", "My first blog post"],
+	["03/02/2019", "post 1", "@pathto(blog/post01)", "第一篇博客"],
 ];
