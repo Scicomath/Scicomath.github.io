b84ab6f31b9145c748b9ff521b37b2ab24981fa3
AI Xin <xin.ai@hairoutech.com>
2020-04-25 00:43:38 +0800

    fix a typo

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,4 +1,4 @@
 var posts_array = [
-    ["04/25/2020", "post 1", "@pathto(blog/post02)", "Large-Scale C++: 声明、定义与Linkage"],
+    ["04/25/2020", "post 2", "@pathto(blog/post02)", "Large-Scale C++: 声明、定义与Linkage"],
     ["04/20/2020", "post 1", "@pathto(blog/post01)", "第一篇博客"],
 ];
