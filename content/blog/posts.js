b0d45e68d27791c1174cf956b69c0f5b5c08b717
AI Xin <xin.ai@hairoutech.com>
2020-04-25 00:41:13 +0800

    add post02

--- content/blog/posts.js
+++ content/blog/posts.js
@@ -1,3 +1,4 @@
 var posts_array = [
-	["03/02/2019", "post 1", "@pathto(blog/post01)", "第一篇博客"],
+    ["04/25/2020", "post 1", "@pathto(blog/post02)", "Large-Scale C++: 声明、定义与Linkage"],
+    ["04/20/2020", "post 1", "@pathto(blog/post01)", "第一篇博客"],
 ];
